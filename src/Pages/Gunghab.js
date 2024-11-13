ad409248285d1fcf4e2095a4f6c69222ee80d528
zsj <cho010105@naver.com>
2024-11-13 15:33:56 +0900

    [ADD] Doughnut Chart 수정 및 선수 궁합도 페이지 css 추가

--- src/Pages/Gunghab.js
+++ src/Pages/Gunghab.js
@@ -1,30 +1,19 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
 import '../styles/LoginPage.css';
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import {auth} from "../firebase";
 
 function gungHab() {
-    const handleGoogleSign = async () => {
-        const provider = new GoogleAuthProvider();
-        await signInWithPopup(auth, provider).then((data) => {
-            console.log(data);
-        }).catch((err) => console.log(err));
-    }
 
     return (
-        <GoogleOAuthProvider clientId="minseoung010105@gmail.com">
-            <div className="login-page-container">
-                <Sidebar />
-                <div className="login-content">
-                    <h2>스포타그램에 오신 선수님, 환영해요!</h2>
-                    <div className="google-login-button">
-                        <button onClick={handleGoogleSign}>Login in Google</button>
-                    </div>
+        <div className="login-page-container">
+            <Sidebar/>
+            <div className="gunghab-content">
+                <div className="page-header">
+                    <h2>선수 궁합도</h2>
+                    <p>선수님과 잘 맞는 선수를 확인할 수 있어요.</p>
                 </div>
             </div>
-        </GoogleOAuthProvider>
+        </div>
     );
 }
 
