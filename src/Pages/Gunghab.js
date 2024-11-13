0aacca41cc51cf02ecc336aab59e03868029daa5
zsj <cho010105@naver.com>
2024-11-13 17:59:33 +0900

    [ADD] 선수 궁합도 데이터 임의 추가 및 css 수정

--- src/Pages/Gunghab.js
+++ src/Pages/Gunghab.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
-import '../styles/LoginPage.css';
+import GunghabTable from '../Table/Gunghab_table';
+import PlayerCard from '../components/PlayerCard';
+import '../styles/Gunghab.css';
 
-function gungHab() {
-
+const Gunghab = () => {
     return (
-        <div className="login-page-container">
-            <Sidebar/>
+        <div className="gunghab-container">
+            <Sidebar />
             <div className="gunghab-content">
                 <div className="page-header">
                     <h2>선수 궁합도</h2>
                     <p>선수님과 잘 맞는 선수를 확인할 수 있어요.</p>
                 </div>
+                <div className="player-cards-container player-cards-left-align">
+                    <PlayerCard name="하트" winRate={75} />
+                    <PlayerCard name="송명기" winRate={25} />
+                </div>
+                <div className="table-container">
+                    <GunghabTable />
+                </div>
             </div>
         </div>
     );
-}
+};
 
-export default gungHab;
+export default Gunghab;
--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/PlayerCard.css';
-// 메인 페이지에 존재하는 사용자 직관 시 최고/최저 승률 선수를 보여주는 컴포넌트
 function PlayerCard({ name, winRate }) {
     return (
         <div className="player-card">
